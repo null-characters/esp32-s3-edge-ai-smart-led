26484b129228e921d820d3e0719cae02205018d1
fengbing <1305913601@qq.com>
2026-04-24 22:04:53 +0800

    chore: 清理Windows/WSL环境，删除不必要的脚本和构建文件
    - 删除WSL专用脚本 (setup-zephyr-wsl.sh, setup-zephyr-env.sh)
    - 删除Windows构建脚本 (build-test.bat)
    - 更新.gitignore忽略.codebuddy目录
    - 清理完成，项目转用Mac开发

--- setup-zephyr-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export ZEPHYR_SDK_INSTALL_DIR=/mnt/e/zephyrproject/zephyr-sdk-0.17.0
-export PATH=$ZEPHYR_SDK_INSTALL_DIR/x86_64-zephyr-elf/bin:$PATH
-echo "SDK: $ZEPHYR_SDK_INSTALL_DIR"
